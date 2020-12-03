847902427008a59cc812b815f9b48c54408e6eea
fredsteam <freddie2test@gmail.com>
2020-12-03 18:50:04 -0500

    Chapter 5

---
+++ Chapter_5/conditional_tests.py
@@ -0,0 +1,15 @@
+car = ['subaru', 'bmw', 'audi', 'honda', 'Toyota']
+print("Is a'subaru' in the list? I predict True.")
+print('subaru' in car)
+
+print("\nIs a 'audi'in the list? I predict True.")
+print('audi' in car)
+
+print("\nIs a 'toyota' in the list? I predict False.")
+print('toyota' in car)
+print("toyota is false because it is capitalized in the list.")
+#print("I will used the .lower with car to have it evaluate all items lowercase.")
+#car.lower() == 'toyota'
+#print("\nIs a 'toyota' in the list? I predict True.")
+#This gave an error because you can't do it this way with a list. You can do it for a
+#single item but not a list. I will probably learn how this can be done later in the book.
